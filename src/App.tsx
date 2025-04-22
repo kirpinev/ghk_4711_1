190f67c8c15db4242b1a03c9a5ba49c502cbd5a7
Igor Kirpinev <kirpinevigor@yandex.ru>
2025-04-22 11:08:13 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -207,7 +207,7 @@ export const App = () => {
         <ButtonMobile
           block
           view="primary"
-          href="alfabank:///sdui_screen?screenName=InvestmentLongread&fromCurrent=true&endpoint=v1/invest-main-screen-view/investment-longread/50219%3flocation=AM%26campaignCode=GH"
+          href="alfabank:///dashboard/alfa_check_cards"
           onClick={submit}
         >
           Подключить
@@ -216,3 +216,7 @@ export const App = () => {
     </>
   );
 };
+
+
+// alfabank:///dashboard/alfa_check_cards
+// alfabank:///sdui_screen?screenName=InvestmentLongread&fromCurrent=true&endpoint=v1/invest-main-screen-view/investment-longread/50219%3flocation=AM%26campaignCode=GH
